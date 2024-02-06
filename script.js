44516a13200466794a4cb50a502f5dd05dd0b99e
Shahzad Minhaj <137610769+Shahzad-Minhaj@users.noreply.github.com>
2024-02-06 09:12:54 +0500

    Second Commit
    Made some changes in the Js file to improve the code.
    Add some more effective styling.
    Add a proper footer to the file.

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 function appendToDisplay(value) {
-    document.getElementById('display').value += value;
-  }
+  document.getElementById('display').value += value;
+}
 
-  function clearDisplay() {
-    document.getElementById('display').value = '';
-  }
+function clearDisplay() {
+  document.getElementById('display').value = '';
+}
 
-  function calculateResult() {
-    try {
-      document.getElementById('display').value = eval(document.getElementById('display').value);
-    } catch (error) {
-      document.getElementById('display').value = 'Error';
-    }
+// function calculateResult() {
+//   try {
+//     const result = eval(document.getElementById('display').value);
+//     document.getElementById('display').value = result;
+//   } catch (error) {
+//     document.getElementById('display').value = 'Error';
+//   }
+// }
+function calculateResult() {
+  try {
+    let input = document.getElementById('display').value;
+
+    // Replace consecutive operators without operands
+    input = input.replace(/(\+{2,}|-{2,}|\*{2,}|\/{2,})/g, (match) => {
+      return match.charAt(0);
+    });
+
+    const result = eval(input);
+    document.getElementById('display').value = result;
+  } catch (error) {
+    document.getElementById('display').value = 'Error';
   }
+}
+
